d1f1831e4f2c3d5e1b4d47c16ed293947356d5b7
Glitch (coffee-productive-nest) <none>
2021-12-23 03:33:34 +0000

    👨🐸 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# [URL Shortener Microservice](https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice)
+# [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-var validUrl=require("valid-url");
-var http = require('http');
-var bodyParser = require("body-parser");
-var middle=bodyParser.urlencoded({extended: false});
-// Basic Configuration
-const port = process.env.PORT || 3000;
+var express = require('express');
+var cors = require('cors');
+var multer=require("multer");
+var upload=multer();
+require('dotenv').config()
+
+var app = express();
 
 app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
+app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-// Your first API endpoint
-var mapper=["nil"];
-app.post('/api/shorturl', middle,function(req, res) {
-  const url=req.body.url;
-  if(validUrl.isHttpsUri(url)){
-     
-  var sUrl=mapper.length;
-  mapper.push(url);
-  res.send({original_url:url,short_url:sUrl});
-  }
-  else{
-    res.send({error:"invalid url"})
-  }
+app.post('/api/fileanalyse',upload.single('upfile'),function (req, res) {
+  const file=req.file;  
+  res.send({name:file.originalname,type:file.mimetype,size:file.size});
 });
-app.get('/api/shorturl/:short',function(req,res){
-  const srt=parseInt(req.params.short);
-  var link=mapper[srt];
-  res.writeHead(301,{Location: link});
-  res.end();
-})
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 });
