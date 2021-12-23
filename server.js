3d3e01dfc520a4f583ace8a2c82bf450277f09aa
Glitch (season-nimble-diploma) <none>
2021-12-23 03:09:06 +0000

    🤘💼 Updated with Glitch

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+var validUrl=require("valid-url");
+var http = require('http');
+var bodyParser = require("body-parser");
+var middle=bodyParser.urlencoded({extended: false});
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -15,10 +18,25 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+var mapper=["nil"];
+app.post('/api/shorturl', middle,function(req, res) {
+  const url=req.body.url;
+  if(validUrl.isHttpsUri(url)){
+     
+  var sUrl=mapper.length;
+  mapper.push(url);
+  res.send({original_url:url,short_url:sUrl});
+  }
+  else{
+    res.send({error:"invalid url"})
+  }
 });
-
+app.get('/api/shorturl/:short',function(req,res){
+  const srt=parseInt(req.params.short);
+  var link=mapper[srt];
+  res.writeHead(301,{Location: link});
+  res.end();
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
